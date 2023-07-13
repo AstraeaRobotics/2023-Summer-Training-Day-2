6389272dc1771510deed82f2e3cedcedf9cbc4af
Heisenberg1525 <sudaykukreja08@gmail.com>
2023-07-13 18:29:15 -0400

    Suday's Commit: I create a drivebase subsystem with 1 motor, and created a command that accesses a method in the drivebase subsystem, and then assign a button (unsure if it works)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,26 +1,23 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj.XboxController;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
+import frc.robot.subsystems.Drivebase;
+
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-
-
+  private final Drivebase drivebase;
+  private final XboxController controller;
+  private final Button buttonB;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    drivebase = new Drivebase();
+    controller = new XboxController(0);
+    buttonB = new JoystickButton(controller, XboxController.Button.kB.value);
+
     // Configure the trigger bindings
     configureBindings();
   }
@@ -39,7 +36,7 @@ public class RobotContainer {
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-
+    buttonB.whenPressed(new ExampleMethodCommand(drivebase));
   }
 
   /**
